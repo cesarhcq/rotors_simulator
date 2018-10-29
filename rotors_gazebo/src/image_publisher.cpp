9a852698ad63db85609f50ea5fbd03327f1939d7
cesar <cesar@fem.unicamp.br>
2018-10-29 18:06:22 -0300

    Aruco init added Opencv

--- rotors_gazebo/src/image_publisher.cpp
+++ rotors_gazebo/src/image_publisher.cpp
@@ -3,6 +3,16 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 
+#include "opencv2/core.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/aruco.hpp"
+#include "opencv2/aruco/dictionary.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/video/tracking.hpp"
+#include "opencv2/features2d/features2d.hpp"
+
 using namespace std;
 using namespace cv;
 // using namespace aruco;
