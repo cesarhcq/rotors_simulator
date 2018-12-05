67d7796b09f43d41e8c49aa4103af9018464af59
Alan Tavares <alan.am.tavares@gmail.com>
2018-12-05 17:30:14 -0200

    init arucoTest

--- rotors_gazebo/scripts/image_sub.py
+++ rotors_gazebo/scripts/image_sub.py
@@ -16,6 +16,10 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+#-- Define Tag\n",
+id_to_find = 1
+marker_size = 40 #-cm
+
 #-- Define the Aruco dictionary\n",
 aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
 parameters =  aruco.DetectorParameters_create()
@@ -34,20 +38,55 @@ class image_converter:
    
   def callback(self,data):
 
+    #-- Get the camera calibration\n",
+    calib_path = '/home/alantavares/aruco_landing_ws/src/rotors_simulator/'
+    camera_matrix = np.loadtxt(calib_path+'cameraMatrix.txt', delimiter = ',')
+    camera_distortion = np.loadtxt(calib_path+'cameraDistortion.txt', delimiter = ',')
+
     try:
       src_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
     except CvBridgeError as e:
       print(e)
-   
-    (rows,cols,channels) = src_image.shape
-    if cols > 60 and rows > 60 :
+
+    #-- Convert in gray scale\n",
+    gray = cv2.cvtColor(src_image, cv2.COLOR_BGR2GRAY) #-- remember, OpenCV stores color images in Blue, Green, Red
+
+    #(rows,cols,channels) = src_image.shape
+    #if cols > 60 and rows > 60 :
       #cv2.circle(src_image, (50,50), 10, 255)
   
-      print('cols = {} rows = {}'.format(cols,rows))
+    #print('cols = {} rows = {}'.format(cols,rows))
+
+    #-- Find all the aruco markers in the image\n",
+    corners, ids, rejected = aruco.detectMarkers(image=gray,
+                                                  dictionary=aruco_dict,
+                                                  parameters=parameters,
+                                                  cameraMatrix=camera_matrix,
+                                                  distCoeff=camera_distortion)
+
+    if ids != None and ids[0] == id_to_find:
+      #-- ret= [rvec,tvec, ?]
+      #-- array of rotation and position of each marker in camera frame
+      #-- rvec = [[rvec_1, [rvec2], ...]]  attitude of the marker respect to camera frame
+      #-- tvec = [[tvec_1, [tvec2], ...]]  position of the marker in camera frame
+      ret = aruco.estimatePoseSingleMarkers(corners, marker_size, camera_matrix, camera_distortion)
+
+      #-- Unpack the output, get only the first\n",
+      rvec, tvec = ret[0][0,0,:], ret[1][0,0,:]
+
+      #-- Draw the detected marker and put a reference frame over it\n",
+      aruco.drawDetectedMarkers(src_image, corners)
+      aruco.drawAxis(src_image, camera_matrix, camera_distortion, rvec, tvec, 40)
       
-      cv2.imshow("Image window", src_image)
+      cv2.imshow("Image-Aruco", src_image)
+      #cv2.imshow("Image-Gray", gray)
       cv2.waitKey(3)
-   
+
+    else:
+      print('Nothing detected')
+      #-- Display the resulting frame\n",
+      cv2.imshow("Image-Aruco",src_image)
+
     #try:
     #  self.image_pub.publish(self.bridge.cv2_to_imgmsg(src_image, "bgr8"))
     #except CvBridgeError as e:
