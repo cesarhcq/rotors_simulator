406d86865b67c9969f26cc02518e09b8128b1a92
Alan Tavares <alan.am.tavares@gmail.com>
2018-12-05 17:53:35 -0200

    test-aruco

--- rotors_gazebo/scripts/image_sub.py
+++ rotors_gazebo/scripts/image_sub.py
@@ -16,14 +16,77 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-#-- Define Tag\n",
-id_to_find = 1
-marker_size = 40 #-cm
+###############################################################################
+#------- ROTATIONS https://www.learnopencv.com/rotation-matrix-to-euler-angles/
 
-#-- Define the Aruco dictionary\n",
-aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
-parameters =  aruco.DetectorParameters_create()
+# Checks if a matrix is a valid rotation matrix.
+def isRotationMatrix(R):
+  #-- transpose the matrix R
+  Rt = np.transpose(R)
 
+  #-- verify if Rt could be identity
+  shouldBeIdentity = np.dot(Rt, R)
+
+  #-- create a identity
+  I = np.identity(3, dtype=R.dtype)
+  n = np.linalg.norm(I - shouldBeIdentity)
+
+  return n < 1e-6
+
+
+# Calculates rotation matrix to euler angles
+# The result is the same as MATLAB except the order
+# of the euler angles ( x and z are swapped ).
+def rotationMatrixToEulerAngles(R):
+    assert (isRotationMatrix(R))
+
+    sy = math.sqrt(R[0, 0] * R[0, 0] + R[1, 0] * R[1, 0])
+
+    singular = sy < 1e-6
+
+    if not singular:
+        x = math.atan2(R[2, 1], R[2, 2])
+        y = math.atan2(-R[2, 0], sy)
+        z = math.atan2(R[1, 0], R[0, 0])
+    else:
+        x = math.atan2(-R[1, 2], R[1, 1])
+        y = math.atan2(-R[2, 0], sy)
+        z = 0
+
+    return np.array([x, y, z])
+
+def update_fps_read():
+    global t_read, fps_read
+    t           = time.time()
+    fps_read    = 1.0/(t - t_read)
+    t_read      = t
+    
+def update_fps_detect():
+    global t_detect, fps_detect
+    t           = time.time()
+    fps_detect  = 1.0/(t - t_detect)
+    t_detect      = t
+
+###############################################################################
+
+#-- Update fps
+def update_fps_read():
+    global t_read, fps_read
+    t           = time.time()
+    fps_read    = 1.0/(t - t_read)
+    t_read      = t
+    
+def update_fps_detect():
+    global t_detect, fps_detect
+    t           = time.time()
+    fps_detect  = 1.0/(t - t_detect)
+    t_detect      = t
+    
+t_read      = time.time()
+t_detect    = t_read
+fps_read    = 0.0
+fps_detect  = 0.0
+    
 ###############################################################################
  
 class image_converter:
@@ -38,10 +101,27 @@ class image_converter:
    
   def callback(self,data):
 
+    #-- Define Tag\n",
+    id_to_find = 1
+    marker_size = 40 #-cm
+
+    #-- Define the Aruco dictionary\n",
+    aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_50)
+    parameters =  aruco.DetectorParameters_create()
+
     #-- Get the camera calibration\n",
     calib_path = '/home/alantavares/aruco_landing_ws/src/rotors_simulator/'
     camera_matrix = np.loadtxt(calib_path+'cameraMatrix.txt', delimiter = ',')
     camera_distortion = np.loadtxt(calib_path+'cameraDistortion.txt', delimiter = ',')
+
+    #-- 180 deg rotation matrix around x axis
+    R_flip = np.zeros((3,3), dtype=np.float)
+    R_flip[0,0] = 1.0
+    R_flip[1,1] = -1.0
+    R_flip[2,2] = -1.0
+
+    #-- Font for the text in the image
+    font = cv2.FONT_HERSHEY_PLAIN
 
     try:
       src_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
@@ -77,6 +157,27 @@ class image_converter:
       #-- Draw the detected marker and put a reference frame over it\n",
       aruco.drawDetectedMarkers(src_image, corners)
       aruco.drawAxis(src_image, camera_matrix, camera_distortion, rvec, tvec, 40)
+
+      #-- Obtain the rotation matrix tag->camera
+      R_ct = np.matrix(cv2.Rodrigues(rvec)[0])
+      R_tc = R_ct.T # function transpose() with '.T'
+
+      #-- Get the attitude in terms of euler 321 (Needs to be flipped first)
+      roll_marker, pitch_marker, yaw_marker = rotationMatrixToEulerAngles(R_tc)
+
+      #-- Now get Position and attitude f the camera respect to the marker
+      pos_camera = -R_tc*np.matrix(tvec).T
+      roll_camera, pitch_camera, yaw_camera = rotationMatrixToEulerAngles(R_flip*R_tc)
+
+
+      #-- Print the tag position in camera frame
+      str_position = "MARKER Position x=%4.0f  y=%4.0f  z=%4.0f "%(tvec[0], tvec[1], tvec[2])
+      cv2.putText(src_image, str_position, (0, 50), font, 1, (255, 255, 0), 1, cv2.LINE_AA)
+      
+      #-- Print the marker's attitude respect to camera frame
+      str_attitude = "MARKER Attitude r=%4.0f  p=%4.0f  y=%4.0f"%(math.degrees(roll_marker),
+        math.degrees(pitch_marker), math.degrees(yaw_marker))
+      cv2.putText(src_image, str_attitude, (0, 100), font, 1, (255, 255, 0), 1, cv2.LINE_AA)
       
       cv2.imshow("Image-Aruco", src_image)
       #cv2.imshow("Image-Gray", gray)
@@ -91,22 +192,6 @@ class image_converter:
     #  self.image_pub.publish(self.bridge.cv2_to_imgmsg(src_image, "bgr8"))
     #except CvBridgeError as e:
     #  print(e)
-
-###############################################################################
-
-def isRotationMatrix(R):
-
-  #-- transpose the matrix R
-  Rt = np.transpose(R)
-
-  #-- verify if Rt could be identity
-  shouldBeIdentity = np.dot(Rt, R)
-
-  #-- create a identity
-  I = np.identity(3, dtype=R.dtype)
-  n = np.linalg.norm(I - shouldBeIdentity)
-
-  return n < 1e-6
 
 ###############################################################################
 
